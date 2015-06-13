3b156f1c0fa3cbe1a2b280c0d7c616cd8f87a9df
Andrei Neagu <neagu.andre@yahoo.it>
2015-06-13 19:15:23 +0200

    updated ip paramter

--- sendfile.py
+++ sendfile.py
@@ -6,12 +6,15 @@ FILENAME = "temperature"
 
 BASE_URL = "http://cryptic-bayou-3624.herokuapp.com/api/"
 
+DEVICE_IP = "192.168.1.7"
+
 with open(FILENAME) as f:
     lines = f.readlines()
     parameters = {}
     for line in lines:
         values = line.strip('\n').split(',')
         parameters[values[0]] = values[1]
+    parameters["ip_address"] = DEVICE_IP
     url = BASE_URL + "send_data?"+urllib.urlencode(parameters)
     print url
     os.system("curl " + url)
