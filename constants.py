3319e605e5a62e56be9a6551ca7cea8935848f1c
Andrei Neagu <neagu.andre@yahoo.it>
2015-06-14 02:40:36 +0200

    changed some commands and log output

--- constants.py
+++ constants.py
@@ -11,15 +11,14 @@ FILENAME = "output.csv"
 
 REAL_DEVICE_IP = "192.168.1.105"
 
-ERROR_MOCK_FILE = """acceleration_y 45.460341
-acceleration_x 17.866871
-tdr 17.957764
-tilt 59.170946
+ERROR_MOCK_FILE = """accelerometer_x 0.20000
+accelerometer_y 0.23000
+tilt 94.0000
+tdr 45.0000
 """
 
-OK_MOCK_FILE = """
-acceleration_y 45.460341
-acceleration_x 17.866871
-tdr 17.957764
-tilt 59.170946
+OK_MOCK_FILE = """accelerometer_x 0.05153
+accelerometer_y -0.02042
+tilt 89.7500
+tdr 49.8924
 """
