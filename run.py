14714808f3478d5d29d96a586156546a42fb3f9c
Andrei Neagu <neagu.andre@yahoo.it>
2015-06-14 07:43:49 +0200

    moved netork status

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ device_network = DeviceNetwork(build_device_list())
 RealDeviceInterfaceThread(device_network)
 
 while True:
+    device_network.print_network_status()
+
     data = raw_input("W (warning event) R (reset network) S (generate real device alarm)")
     if data.upper() == "W":
         device_network.set_alarm_status_and_warn_near_devices(ALARMED_DEVICE)
@@ -19,5 +21,3 @@ while True:
 
     if data.upper() == "S":
         generate_real_device_event(ERROR_MOCK_FILE)
-
-    device_network.print_network_status()
