c0042596e0a5efa34162803bb0af7bd7d1f9aba5
Andrei Neagu <neagu.andre@yahoo.it>
2015-06-14 03:09:47 +0200

    push debug

--- device_interface.py
+++ device_interface.py
@@ -14,6 +14,7 @@ def get_parameters_from_file(filename):
         parameters = {}
         for line in lines:
             values = line.strip('\n').split(',')
+            print values
             parameters[values[0]] = values[1]
     return parameters
 
